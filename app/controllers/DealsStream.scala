b9a023fd870224474a629d5f08cb45f016c60e96
Tobias Neef <tobnee@gmail.com>
2013-03-26 22:37:34 +0100

    multi city feed

--- app/controllers/DealsStream.scala
+++ app/controllers/DealsStream.scala
@@ -11,27 +11,41 @@ import play.api.libs.json.{JsValue, Json}
 import Json._
 
 object DealsStream extends Controller {
-
-  def dealFeedView(city: String) = Action {
-    Ok(views.html.dealStream(city))
+  val asJson: Enumeratee[(String, String), JsValue] = Enumeratee.map[(String, String)] {
+    case (desc, percent) => toJson(Map("desc" -> toJson(desc), "percent" -> toJson(percent)))
   }
 
-  def dealFeed(city: String) = Action {
-    val asJson: Enumeratee[(String, String), JsValue] = Enumeratee.map[(String, String)] {
-      case (desc, percent) => toJson(Map("desc" -> toJson(desc), "percent" -> toJson(percent)))
+  def dealFeedView() = Action {
+    Ok(views.html.dealStream())
+  }
+
+  def dealFeed() = Action {
+    Async {
+      buildCityEventProducers.map(enum => Ok.feed(enum through asJson.compose(EventSource())).as("text/event-stream"))
     }
-    Ok.feed(dealEvents(city) through asJson.compose(EventSource())).as("text/event-stream")
+  }
+
+  def buildCityEventProducers: Future[Enumerator[(String, String)]] = {
+    Groupon.supportedCities().map(a =>
+      a.map {
+        case (city, link) =>
+          link.split("/").last
+      }.map(dealEvents).reduce {
+        (a, b) =>
+          a >- b
+      }
+    )
   }
 
   def dealEvents(city: String): Enumerator[(String, String)] = {
-    val deals = Groupon.dealLinksForCity(city).map(_.map(Groupon.dealData))
-    val d = Await.result(deals.fallbackTo(Future(Nil)), Timeout(1000).duration).toIterator
+    val deals = Groupon.dealLinksForCity(city).map(_.toStream.map(Groupon.dealData))
+    val d = Await.result(deals.fallbackTo(Future(Stream())), Timeout(5000).duration).toIterator
     Enumerator.generateM {
       if (d.hasNext) {
         // build empty event if no desc is given
         val f = d.next().map(_.orElse(Some("", "")))
         // time messages to be delivered with a maximum rate
-        f.flatMap(content => play.api.libs.concurrent.Promise.timeout(content, 500))
+        f.flatMap(content => play.api.libs.concurrent.Promise.timeout(content, (20000*math.random).toLong))
       }
       else Future(None)
     }
